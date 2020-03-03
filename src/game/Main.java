311a2d7f8d7c4c6fd19eb1c8d2b7ebb6c01f22f5
alexsenden <alexandersenden@gmail.com>
2020-03-02 20:16:35 -0800

    Initial Commit

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,7 +3,13 @@ package game;
 public class Main {
 	public static void main(String[] args) {
 		Game game = new Game();
+		
 		GraphicsThread graphicsThread = new GraphicsThread(game);
+		GameThread gameThread = new GameThread(game);
+		AudioThread audioThread = new AudioThread(game);
+		
 		graphicsThread.start();
+		gameThread.start();
+		audioThread.start();
 	}
 }
